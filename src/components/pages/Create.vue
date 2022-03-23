908d73e3c5e4b8ec7157810aca88d0d47108795c
AntoineMorey <85560122+AntoineMorey@users.noreply.github.com>
2022-03-23 10:02:45 +0100

    form edit et add

--- src/components/pages/Create.vue
+++ src/components/pages/Create.vue
@@ -1,3 +1,11 @@
 <template>
-  Create
+  <form>
+    <p><input type="text" placeholder="nom" required></p>
+    <p><input type="text" placeholder="type" required></p>
+    <textarea name="message" rows="10" cols="22" placeholder="description" required></textarea>
+    <p><input type="text" placeholder="price" required></p>
+    <p><input type="text" placeholder="alcohol" required></p>
+    <p><input type="file" required></p>
+    <button type="submit"> ajouter </button>
+  </form>
 </template>
--- src/components/pages/Edit.vue
+++ src/components/pages/Edit.vue
@@ -1,3 +1,11 @@
 <template>
-  Edit
+  <form>
+    <p><input type="text" value="nom" required></p>
+    <p><input type="text" value="type" required></p>
+    <textarea name="message" rows="10" cols="22" required>description</textarea>
+    <p><input type="text" value="price" required></p>
+    <p><input type="text" value="alcohol" required></p>
+    <p><input type="file" required></p>
+    <button type="submit"> ajouter </button>
+  </form>
 </template>
