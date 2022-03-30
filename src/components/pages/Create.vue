e79188b3005ee8d76a7ae4f123a615156358eefc
AntoineMorey <85560122+AntoineMorey@users.noreply.github.com>
2022-03-30 09:22:51 +0200

    create fini

--- src/components/pages/Create.vue
+++ src/components/pages/Create.vue
@@ -32,8 +32,8 @@ export default {
     <p><input type="text" placeholder="name" v-model="name" required></p>
     <p><input type="text" placeholder="type" v-model="type" required></p>
     <textarea rows="10" cols="22" placeholder="description" v-model="description" required></textarea>
-    <p><input type="text" placeholder="price" v-model="price" required></p>
-    <p><input type="text" placeholder="alcohol" v-model="alcohol" required></p>
+    <p><input type="number" step="0.01" placeholder="price" v-model="price" required></p>
+    <p><input type="number" max="100" placeholder="alcohol" v-model="alcohol" required></p>
     <!-- <p><input type="file" accept="image/*" required></p> -->
     <button type="submit"> ajouter </button>
   </form>
